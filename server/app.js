83dd8a1e59d126d801913c0a7aa4f9d24eaaab50
Susan Rehl <susanrehl@gmail.com>
2016-06-08 11:02:58 -0500

    took out server and function

--- server/app.js
+++ server/app.js
@@ -2,19 +2,19 @@ var http=require('http');
 var express=require('express');
 var app=express();
 
-http.createServer(function(req, res) {
-  console.log('hi from server');
-  res.writeHead(200);
-  res.write('hi folks from server response');
-  res.write(responseFunction());
-  res.end();
-}).listen(3000);
+// http.createServer(function(req, res) {
+//   console.log('hi from server');
+//   res.writeHead(200);
+//   res.write('hi folks from server response');
+//   res.write(responseFunction());
+//   res.end();
+// }).listen(3000);
 
 console.log('hello world on port 3000');
 
-var responseFunction = function(){
-  return('from responseFunction');
-};
+// var responseFunction = function(){
+//   return('from responseFunction');
+// };
 
 var server=app.listen(process.env.PORT || 3000, function() {
   console.log('hello world from port 3000');
