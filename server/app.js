302e7c0ed4a70349cae3e3e35d515d4b522023d3
Susan Rehl <susanrehl@gmail.com>
2016-06-08 11:19:40 -0500

    fixed bug

--- server/app.js
+++ server/app.js
@@ -16,11 +16,11 @@ var responseFunction = function(){
   return('from responseFunction');
 };
 
-app.get('/'), function(req, res) {
+app.get('/', function(req, res) {
   res.writeHead(200);
   res.write('hello world from the base url');
   res.end();
-};
+});
 
 var server=app.listen(process.env.PORT || 3000, function() {
   console.log('hello world from port 3000');
